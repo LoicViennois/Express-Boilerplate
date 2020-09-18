c449c7a19f6d0d8a73d2c6a2d919d5acb174672d
Loïc Viennois <contact@loicviennois.com>
2020-09-18 14:24:43 +0200

    update tslint rules

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,12 +1,12 @@
-import * as request from 'supertest'
-import { Response } from 'supertest'
-import { app } from './app'
+import * as request from 'supertest';
+import { Response } from 'supertest';
+import { app } from './app';
 
 describe('app', () => {
   test('should answer to the GET method', async () => {
-    const res: Response = await request(app).get('/')
-    expect(res.status).toBe(200)
-    expect(res.text).toBe('hello world')
-  })
-})
+    const res: Response = await request(app).get('/');
+    expect(res.status).toBe(200);
+    expect(res.text).toBe('hello world');
+  });
+});
 
--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,20 @@
-import { Request, Response } from 'express'
-import * as express from 'express'
-import * as helmet from 'helmet'
-import * as morgan from 'morgan'
+import * as express from 'express';
+import { Request, Response } from 'express';
+import * as helmet from 'helmet';
+import * as morgan from 'morgan';
 
-import { healthCheck } from './server/routes/healthz'
+import { healthCheck } from './server/routes/healthz';
 
-export const app = express()
+export const app = express();
 
 // TODO: implement CORS
 
-app.use(helmet())
-app.use(morgan('tiny'))
+app.use(helmet());
+app.use(morgan('tiny'));
 
 app.get('/', (req: Request, res: Response) => {
-  res.status(200).send('hello world')
-})
+  res.status(200).send('hello world');
+});
 
 // Docker HEALTHCHECK
-app.route('/healthz').get(healthCheck)
+app.route('/healthz').get(healthCheck);
