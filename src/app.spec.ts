01d051cb5a234b97a775406599cb55b695cad39e
Loïc Viennois <contact@loicviennois.com>
2020-09-23 09:28:28 +0200

    fix test

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -4,7 +4,7 @@ import { app } from './app';
 
 describe('app', () => {
   test('should answer to the GET method', async () => {
-    const res: Response = await request(app).get('/');
+    const res: Response = await request(app).get('/hello');
     expect(res.status).toBe(200);
     expect(res.text).toBe('hello world');
   });
